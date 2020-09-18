300833fa61c2a1248a6c735c8db0b8be236a5a8d
mostafa abdo <abdelbaky.mostafa@gmail.com>
2020-09-17 21:16:42 -0500

    refactor and update categorize sites

--- src/scripts/categorize_site.py
+++ src/scripts/categorize_site.py
@@ -5,6 +5,7 @@ import os
 # Third-Party Libraries
 from dotenv import load_dotenv
 import requests
+from utils.message_handling import error_msg, success_msg
 
 # Load environment variables from .env file
 load_dotenv()
@@ -15,37 +16,42 @@ API_KEY = os.environ.get("API_KEY")
 auth = {"api_key": API_KEY}
 
 
-def get_live_site_list():
+def get_live_sites():
     """Returns a list of active websites."""
     resp = requests.get(f"{URL}/api/live-sites/", headers=auth)
     return resp.json()
 
 
-def categorize_live_site(live_site_id):
+def categorize_live_site():
     """
     Categorize an active site.
 
     Check if the domain has already been categorized.
     Categorize the domain on multiple proxies.
     """
-    resp = requests.get(f"{URL}/api/categorize/{live_site_id}/", headers=auth)
-
-    return resp.json()
-
-
-if __name__ == "__main__":
-    # Pull available live sites
-    live_sites = get_live_site_list()
-
     # Define the desired site name from list
     site_name = input("Please enter site name: ")
 
     # Access live site data by uuid
     live_site_id = "".join(
-        site.get("_id") for site in live_sites if site_name == site.get("name")
+        site.get("_id")
+        for site in get_live_sites()
+        if site_name == site.get("domain").get("Name")
     )
 
-    # Categorize site
-    categorized = categorize_live_site(live_site_id=live_site_id)
+    resp = requests.get(f"{URL}/api/categorize/{live_site_id}/", headers=auth)
 
-    print(categorized)
+    try:
+        resp.raise_for_status()
+    except requests.exceptions.HTTPError as e:
+        error_msg(str(e))
+        return
+
+    resp_json = resp.json()
+
+    if resp_json.get("error"):
+        error_msg(resp_json["error"])
+    else:
+        success_msg(resp_json["message"])
+
+    return resp_json
