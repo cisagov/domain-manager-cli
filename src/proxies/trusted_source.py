d22e2af7dc0555cf3c0276030806cdcf21d7b12f
mostafa abdo <abdelbaky.mostafa@gmail.com>
2020-09-29 20:20:09 -0500

    update categorization scripts

--- src/proxies/trusted_source.py
+++ src/proxies/trusted_source.py
@@ -3,7 +3,7 @@
 # Third-Party Libraries
 from selenium.webdriver.common.by import By
 
-driver.get(url)
+driver.get("https://trustedsource.org/")
 driver.set_window_size(2061, 1265)
 driver.find_element(By.NAME, "product").click()
 dropdown = driver.find_element(By.NAME, "product")
--- src/test_harness/categorize.py
+++ src/test_harness/categorize.py
@@ -6,31 +6,25 @@
 import os
 
 # Third-Party Libraries
-from api.documents.active_site import ActiveSite
-from api.documents.proxy import Proxy
-from bson.son import SON
 from dotenv import load_dotenv
-from flask import current_app
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 
 # Load environment variables from .env file
 load_dotenv()
 api_key = os.getenv("TWO_CAPTCHA")
 
 
-def categorization_manager(live_site_id):
-    """Manage categorization of active sites."""
+def categorization_manager(domain_url):
+    """Categorize site with all proxies in proxies folder."""
 
-    is_submitted = []
     # Submit domain to proxy
-    proxies = Proxy.get_all()
+    proxies = os.listdir("../proxies")
     for proxy in proxies:
         try:
             driver = webdriver.Chrome(executable_path="../../drivers/chromedriver")
             exec(
-                proxy.get("script").decode(),
-                {"driver": driver, "url": proxy.get("url"), "domain": domain_url},
+                open(f"../proxies/{proxy}").read(),
+                {"driver": driver, "domain": domain_url},
             )
             driver.quit()
         except Exception as err:
@@ -40,8 +34,5 @@ def categorization_manager(live_site_id):
     # Quit WebDriver
     driver.quit()
 
-    # Update database
-    ActiveSite.update(live_site_id=live_site_id, is_submitted=is_submitted)
-    return {
-        "message": f"{domain} has been successfully categorized with Bluecoat, Fortiguard and McAfee"
-    }
+
+categorization_manager("example.com")
