33dbafea6a71efcf85afceb8def91c466b001c92
mostafa abdo <abdelbaky.mostafa@gmail.com>
2020-10-11 23:34:09 -0400

    make hosted zones unique, avoid duplicates

--- src/dns_record_handler/hosted_zone_generator.py
+++ src/dns_record_handler/hosted_zone_generator.py
@@ -6,8 +6,6 @@ import time
 # Third-Party Libraries
 import boto3
 
-# NOTE You cannot created hosted zones for TLD (Top Level Domains such as .com)
-
 
 def generate_hosted_zone(domain_name):
     """
@@ -17,15 +15,33 @@ def generate_hosted_zone(domain_name):
     """
     route53 = boto3.client("route53")
 
+    hosted_zone_list = route53.list_hosted_zones()["HostedZones"]
+
+    hosted_zone_names = [hosted_zone.get("Name") for hosted_zone in hosted_zone_list]
+
+    if f"{domain_name}." in hosted_zone_names:
+        hosted_zone_id = "".join(
+            hosted_zone.get("Id")
+            for hosted_zone in hosted_zone_list
+            if hosted_zone.get("Name") == "signalsquared.com."
+        )
+        hosted_zone = route53.get_hosted_zone(Id=hosted_zone_id)
+        return "\n".join(
+            nameserver for nameserver in hosted_zone["DelegationSet"]["NameServers"]
+        )
+
     # used as unique identifier generation
     # every hosted zone must have unique identifer
     unique_identifier = time.asctime()
-    create_zone = route53.create_hosted_zone(
+
+    hosted_zone = route53.create_hosted_zone(
         Name=domain_name,
         CallerReference=unique_identifier,
     )
 
-    return "".join(create_zone["DelegationSet"]["NameServers"])
+    return "\n".join(
+        nameserver for nameserver in hosted_zone["DelegationSet"]["NameServers"]
+    )
 
 
 if __name__ == "__main__":
@@ -33,7 +49,7 @@ if __name__ == "__main__":
     parser.add_argument(
         "-d",
         "--domain",
-        help="enter a qualified domain such as www.example.com",
+        help="enter a qualified domain such as www.example.com to create a hosted zone",
         type=str,
     )
     args = parser.parse_args()
