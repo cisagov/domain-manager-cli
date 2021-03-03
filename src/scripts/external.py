c9a2ab5029f01c01ff289e3e21a302534eb5ac25
mostafa abdo <abdelbaky.mostafa@gmail.com>
2021-03-02 19:59:46 -0600

    initialize external script.

---
+++ src/scripts/external.py
@@ -0,0 +1,9 @@
+"""External domains not managed by DM."""
+# Third-Party Libraries
+import click
+
+
+@click.group()
+def external():
+    """Manage categorization of external websites."""
+    pass
