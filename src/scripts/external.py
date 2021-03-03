99f60b9778b82316fcd32ef2051361a628c55c30
mostafa abdo <abdelbaky.mostafa@gmail.com>
2021-03-02 21:06:05 -0600

    add check category on external sites

--- src/scripts/external.py
+++ src/scripts/external.py
@@ -1,9 +1,29 @@
 """External domains not managed by DM."""
 # Third-Party Libraries
 import click
+import requests
+from utils.message_handling import error_msg, success_msg
+from utils.settings import URL, auth
 
 
 @click.group()
 def external():
     """Manage categorization of external websites."""
     pass
+
+
+@external.command("check")
+@click.option("-d", "--domain", required=True, help="Enter a domain name")
+def check_category(domain):
+    """Check category on an unmanaged website."""
+    resp = requests.get(f"{URL}/api/categories/{domain}/external/", headers=auth)
+
+    try:
+        resp.raise_for_status()
+    except requests.exceptions.HTTPError as e:
+        error_msg(str(e))
+        return
+
+    for proxy in resp.json():
+        success_msg("".join(f"{key}: {value}" for key, value in proxy.items()))
+    return resp.text
