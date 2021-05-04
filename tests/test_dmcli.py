3d001d1ae4ce97804d0d03c3088f00a955fedd23
Zachary Wilson <zachary.wilson@inl.gov>
2021-05-04 14:33:05 -0600

    add cli version test

---
+++ tests/test_dmcli.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env pytest -vs
+"""Tests for example."""
+
+# Standard Python Libraries
+import sys
+from unittest.mock import patch
+
+# Third-Party Libraries
+import pytest
+
+# cisagov Libraries
+import dmcli
+from dmcli import main
+
+PROJECT_VERSION = dmcli.__version__
+
+
+def test_stdout_version(capsys):
+    """Verify that version string sent to stdout agrees with the module version."""
+    with pytest.raises(SystemExit):
+        with patch.object(sys, "argv", ["bogus", "--version"]):
+            main.start()
+    captured = capsys.readouterr()
+    assert (
+        PROJECT_VERSION in captured.out
+    ), "standard output by '--version' should agree with module.__version__"
