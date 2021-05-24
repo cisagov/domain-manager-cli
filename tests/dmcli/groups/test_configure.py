163e7b77883fbd0687bd1cf84c06c8b176ce4f31
mostafa abdo <abdelbaky.mostafa@gmail.com>
2021-05-23 19:44:44 -0500

    configure command: remove brackets

--- tests/dmcli/groups/test_configure.py
+++ tests/dmcli/groups/test_configure.py
@@ -15,7 +15,7 @@ def test_configure_command(mocker, capsys):
     result = runner.invoke(configure, input="\n".join(["test.url", "test_api_key"]))
 
     result_regex = re.compile(
-        "Enter Domain Manager Url \\[.*\\]: test.url\nEnter API Key \\[.*\\]: test_api_key\n"
+        "Enter Domain Manager Url.*: test.url\nEnter API Key.*: test_api_key\n"
     )
     assert not result.exception
     assert result_regex.match(result.output) is not None
