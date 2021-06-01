9e536b732d2204f0f738e4a0aba8aab47d29cbad
mostafa abdo <abdelbaky.mostafa@gmail.com>
2021-06-01 12:09:17 -0700

    add external categorize unit test

--- tests/dmcli/groups/test_external.py
+++ tests/dmcli/groups/test_external.py
@@ -26,3 +26,27 @@ def test_external_check_command(mocker):
     result = runner.invoke(external, "check", input="example.com")
 
     assert result.output.splitlines()[0] == "Domain name: example.com"
+
+
+def test_external_categorize_command(mocker):
+    """Test `dmcli external categorize`."""
+    mocker.patch(
+        "dmcli.utils.api.get",
+        return_value=[
+            {
+                "name": "example.com",
+                "categories": ["category-1", "category-2", "category-3"],
+            }
+        ],
+    )
+
+    mocker.patch(
+        "dmcli.utils.api.post",
+        return_value="Submitted for categorization.",
+    )
+    runner = CliRunner()
+    result = runner.invoke(
+        external, "categorize", input="\n".join(["example.com", "category-1"])
+    )
+
+    assert result.output.splitlines()[0] == "Domain name: example.com"
