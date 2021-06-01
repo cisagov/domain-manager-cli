44ce8a9c63a0c25073c0a3f74a7101fbd8a268a1
mostafa abdo <abdelbaky.mostafa@gmail.com>
2021-06-01 11:51:16 -0700

    add external base usage unit test'

--- tests/dmcli/groups/test_external.py
+++ tests/dmcli/groups/test_external.py
@@ -1,1 +1,16 @@
 """External group tests."""
+# Third-Party Libraries
+from click.testing import CliRunner
+
+# cisagov Libraries
+from dmcli.groups.external import external
+
+
+def test_external_group_command():
+    """Test `dmcli external`."""
+    runner = CliRunner()
+    result = runner.invoke(external)
+
+    assert (
+        result.output.splitlines()[0] == "Usage: external [OPTIONS] COMMAND [ARGS]..."
+    ), "base external command not outputting usage."
