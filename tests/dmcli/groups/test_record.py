4f4c42e7f9d0b66d39dc412eb08fa5d4abace952
mostafa abdo <abdelbaky.mostafa@gmail.com>
2021-06-02 17:04:53 -0700

    add record get unit test

--- tests/dmcli/groups/test_record.py
+++ tests/dmcli/groups/test_record.py
@@ -14,3 +14,45 @@ def test_record_group_command():
     assert (
         result.output.splitlines()[0] == "Usage: record [OPTIONS] COMMAND [ARGS]..."
     ), "base record command not outputting usage."
+
+
+def test_record_get_command(mocker):
+    """Test `dmcli record get`."""
+    mocker.patch(
+        "dmcli.utils.api.get",
+        return_value=[{"name": "example.com"}],
+    )
+    runner = CliRunner()
+    result = runner.invoke(record, "get", input="example.com")
+
+    assert result.output.splitlines()[0] == "Domain: example.com"
+
+
+# def test_record_add_command():
+#     """Test `dmcli record add`."""
+#     runner = CliRunner()
+#     result = runner.invoke(record)
+
+#     assert (
+#         result.output.splitlines()[0] == "Usage: record [OPTIONS] COMMAND [ARGS]..."
+#     ), "base record command not outputting usage."
+
+
+# def test_record_delete_command():
+#     """Test `dmcli record delete`."""
+#     runner = CliRunner()
+#     result = runner.invoke(record)
+
+#     assert (
+#         result.output.splitlines()[0] == "Usage: record [OPTIONS] COMMAND [ARGS]..."
+#     ), "base record command not outputting usage."
+
+
+# def test_record_update_command():
+#     """Test `dmcli record update`."""
+#     runner = CliRunner()
+#     result = runner.invoke(record)
+
+#     assert (
+#         result.output.splitlines()[0] == "Usage: record [OPTIONS] COMMAND [ARGS]..."
+#     ), "base record command not outputting usage."
